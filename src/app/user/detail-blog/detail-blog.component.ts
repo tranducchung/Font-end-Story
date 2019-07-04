3a8b31d8784088be89045cf7b32658dadd72ee78
nguyenanh <nguyenanhtran2406@gmail.com>
2019-07-04 14:01:22 +0700

    ng anh test

--- src/app/user/detail-blog/detail-blog.component.ts
+++ src/app/user/detail-blog/detail-blog.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Blog, User} from '../ipost';
 import {PostService} from '../../service/post.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../../service/user.service';
+import {TokenService} from '../../auth/token.service';
 
 @Component({
   selector: 'app-detail-blog',
@@ -13,24 +14,36 @@ import {UserService} from '../../service/user.service';
 export class DetailBlogComponent implements OnInit {
   blog: Blog;
   listUser: User[];
+  info: any;
+
   constructor(
     private postService: PostService,
     private route: ActivatedRoute,
-    private userService: UserService
-  ) {}
+    private userService: UserService,
+    private tokenService: TokenService
+  ) {
+  }
+
   ngOnInit() {
+    this.info = {
+      token: this.tokenService.getToken(),
+      username: this.tokenService.getUsername(),
+      email: this.tokenService.getEmail(),
+      authorities: this.tokenService.getAuthor()
+    };
     const id = +this.route.snapshot.paramMap.get('id');
-    this.postService.getBlogById(id).subscribe( next => {
+    this.postService.getBlogById(id).subscribe(next => {
       this.blog = next;
     }, error => {
       console.log(error);
       this.blog = null;
-    } );
-    this.userService.getUsers().subscribe(next => (this.listUser = next), error => (this.listUser = []) );
+    });
+    this.userService.getUsers().subscribe(next => (this.listUser = next), error => (this.listUser = []));
   }
+
   shareBlog(idUser: number, idBlog: number) {
     console.log(idUser);
     console.log(idBlog);
-    this.userService.shareBlog(idUser, idBlog).subscribe(next => console.log('aaa' + next), error => console.log(error) );
+    this.userService.shareBlog(idUser, idBlog).subscribe(next => console.log('aaa' + next), error => console.log(error));
   }
 }
